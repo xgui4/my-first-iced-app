38e7f487ea94c04e51f8e58bb80770a11d876611
xgui4 <134389196+xgui4@users.noreply.github.com>
2025-09-18 15:47:07 -0400

    2025-09-18

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf, vec};
+use std::{env::current_dir, fs, path::PathBuf, vec};
 
 use iced::{widget::{button, column, row, text}, window, Element, Length::Fill, Task};
 
@@ -21,12 +21,18 @@ impl Default for AppState  {
 
 #[derive(Debug, Clone)]
 enum Message {
-    Exit
+    Exit,
+    CD(PathBuf)
 }
 
 fn update(state : &mut AppState, message:Message) -> Task<Message> {
     match message {
         Message::Exit => window::get_latest().and_then(window::close),
+        Message::CD(path_buf) => {
+            state.current_dir = path_buf;
+            state.current_files = get_files(&state.current_dir);
+            Task::none()
+        }
     }
 }
 
@@ -34,9 +40,14 @@ fn view(state : &AppState) -> Element<Message> {
     let mut content = column![row![        
         text(state.current_dir.to_str().unwrap_or("Unknow Directory"))
         .size(32).width(Fill),
-        button(text("Up").size(24)).on_press(Message::Exit),
-        button(text("Exit").size(24)).on_press(Message::Exit)
-    ].spacing(8)]; 
+        button(text("Up").size(24)).on_press(Message::CD(
+            state.current_dir.parent().unwrap_or(&state.current_dir).to_path_buf()
+        )),
+        button(text("Exit").size(24)).on_press(Message::Exit),
+    ]
+    .spacing(8)]
+    .spacing(2)
+    .padding(4); 
 
     for file in &state.current_files {
         content = content.push(text(&file.0))
@@ -72,4 +83,4 @@ fn get_files(path: &PathBuf) -> Vec<(String, bool)> {
     dirs
 }
 
-// Source : https://youtu.be/2CQ4hLB2IMw?si=AivLczXcm-ryd2eG&t=1125
+// Source https://youtu.be/2CQ4hLB2IMw?si=G2kkdJBWK6viY2so&t=1164
